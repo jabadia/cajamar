edaaff7dce9ba1e5253a98266732a4c6fec16fb1
Javier Abadía <javi.abadia@gmail.com>
2016-10-03 09:51:26 +0200

    servir datos en csv desde un df de pandas

--- client/app.js
+++ client/app.js
@@ -3,9 +3,27 @@
 
 var app = angular.module('app', []);
 
-app.controller('MainCtrl', function($scope)
+app.service('backendApi', function($http)
 {
-
+	return {
+		getCsv: function()
+		{
+			return $http.get('/query_csv').then(function(response)
+			{
+				return response.data;
+			});
+		},
+	};
+});
+	
+app.controller('MainCtrl', function($scope, backendApi)
+{
+	$scope.loading = true;
+	backendApi.getCsv().then(function(csv)
+	{
+		$scope.csv = csv;
+		$scope.loading = false;
+	});
 });
 
 }())
--- server/server.py
+++ server/server.py
@@ -1,14 +1,38 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import json
 import os
 from datetime import datetime
+import time
 
 from functools import wraps, update_wrapper
+
+import pandas
 
 from flask import Flask, redirect, request, send_from_directory, make_response
 from werkzeug.utils import secure_filename
 
+# globals
 app = Flask(__name__)
+data = None
+
+def init():
+    global data
+    datafile = '../data/cards.txt'
+    # fichero cards.txt recortado para desarrollar con más agilidad
+    # $ head -300 cards.txt >head.txt
+    # datafile = '../data/head.txt'   
+    print "reading data"
+    t0 = time.time()
+    data = pandas.read_csv(datafile, sep='|', header=0, parse_dates=['DIA'], decimal=',')
+    t1 = time.time()
+    print "ok %.2fseg" % (t1-t0)
+
+    # enrich data
+    data['DIA_SEMANA'] = data['DIA'].dt.dayofweek # 0 = lunes
+    data['MES'] = data['DIA'].dt.month
+    data['IMPORTE_MEDIO'] = data['IMPORTE'] / data['NUM_OP']
 
 
 # from http://arusahni.net/blog/2014/03/flask-nocache.html
@@ -25,6 +49,7 @@ def nocache(view):
     return update_wrapper(no_cache, view)
 
 
+
 @app.route('/')
 def index():
     return redirect('/index.html')
@@ -34,13 +59,16 @@ def index():
 def send_static(path):
     return send_from_directory('../client', path)
 
-@app.route('/query_data')
-def query_data():
 
-    return json.dumps({
-        'msg': "ok",
-    })
+@app.route('/query_csv')
+def query_csv():
+    # aqui se pueden filtrar o procesar los datos antes de devolverlos
+    sector = request.args.get('sector') or 'MODA Y COMPLEMENTOS'
+    result = data if sector == '*' else data[data['SECTOR']== sector] 
+    result = result[0:100]
+    return result.to_csv(index=False, sep='|', float_format="%.2f")
 
 
 if __name__ == "__main__":
+    init()
     app.run(debug=True, port=5001)
